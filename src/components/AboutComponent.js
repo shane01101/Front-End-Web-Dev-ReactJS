0c0f06fe990cd089227404b43495884f22e9841b
Shane Honanie <shaneh425@gmail.com>
2018-10-15 13:23:46 -0700

    Assignment 02v2

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
-import { CardImg, CardTitle, CardSubtitle, CardText} from 'reactstrap'
 import { Link } from 'react-router-dom';
 
 function RenderLeader({leader}) {
